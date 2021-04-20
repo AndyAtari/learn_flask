73c9dedaa39af2811d7a466a628077979013e172
Andrew Alteri <andrewsalteri@gmail.com>
2021-04-19 20:43:40 -0500

    Debugging and properly extending layout.

--- env/app.py
+++ env/app.py
@@ -1,5 +1,5 @@
 from flask import Flask
-from flask import render_template
+from flask import Flask, render_template
 from datetime import datetime 
 import re
 
@@ -7,8 +7,15 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return "Hello, Flask!"
+    return render_template("home.html")
 
+@app.route("/about/")
+def about():
+    return render_template("about.html")
+
+@app.route("/contact/")
+def contact():
+    return render_template("contact.html")
 
 @app.route("/hello/")
 @app.route("/hello/<name>")
