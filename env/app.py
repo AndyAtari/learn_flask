fe2d0478c6798bbcff847a83e4614848a9c6846a
Andrew Alteri <andrewsalteri@gmail.com>
2021-04-20 13:35:43 -0500

    Added resume route and link in header.

--- env/app.py
+++ env/app.py
@@ -13,6 +13,10 @@ def home():
 def about():
     return render_template("about.html")
 
+@app.route("/resume/")
+def resume():
+    return render_template("resume.html")
+    
 @app.route("/contact/")
 def contact():
     return render_template("contact.html")
