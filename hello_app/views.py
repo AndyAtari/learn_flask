c4dbf83b4dbb0cc8586a675ec0f0a1c20a3b12da
Andrew Alteri <andrewsalteri@gmail.com>
2021-04-21 19:25:20 -0500

    WIP. Refactoring for separating concerns.

---
+++ hello_app/views.py
@@ -0,0 +1,35 @@
+import re
+from datetime import datetime
+
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    return render_template("home.html")
+
+@app.route("/about/")
+def about():
+    return render_template("about.html")
+
+@app.route("/resume/")
+def resume():
+    return render_template("resume.html")
+    
+@app.route("/contact/")
+def contact():
+    return render_template("contact.html")
+
+@app.route("/hello/")
+@app.route("/hello/<name>")
+def hello_there(name = None):
+    return render_template(
+        "hello_there.html", 
+        name=name,
+        date=datetime.now()
+    )
+    
+@app.route("/api/data")
+def get_data():
+    return app.send_static_file("data.json")
